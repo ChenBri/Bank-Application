3cb34f3a2ae273ce43d4861602b0fd340ff8368e
Chen <chenbrilling@gmail.com>
2024-08-19 14:47:03 +0300

    Converted events to json

--- Front/src/Components/Pages/AdminDashboard.tsx
+++ Front/src/Components/Pages/AdminDashboard.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -21,6 +20,7 @@ import GetUser from './AdminEvents/GetUserEvent';
 import format from './../../formattingUtils' ;
 import IconResolver from './AdminEvents/IconResolver';
 
+import events from './AdminEvents/events.json';
 
 const drawerWidth = 240;
 
@@ -32,25 +32,7 @@ export default function ResponsiveDrawer() {
         setCurrentEvent(e);
     }
 
-    let events: any =
-        [
-            {
-                text: "Main Menu",
-                operation: "MAIN",
-                iconName: "Home"
-            },
-            {
-                text: "Get User",
-                operation: "GET_USER",
-                iconName: "Person"
-            },
-            {
-                text: "Get Users",
-                operation: "GET_USERS",
-                iconName: "People"
-            }
-        ]
-
+    
         
 
     const drawer = (
